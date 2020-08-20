4766bd197c70948b708e5eb1fa6425f235449cfd
Alena Gontarenko <gontarenko.alena@gmail.com>
2020-08-20 14:12:44 +0300

    add MainController

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'MainController@index');
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/categories','MainContoller@categories');
 
-
-Route::get('/categories', function () {
-    return view('categories');
-});
-
-Route::get('/product/iphone_x_64', function () {
-    return view('product');
-});
+Route::get('/product/iphone_x_64', 'MainController@product');
