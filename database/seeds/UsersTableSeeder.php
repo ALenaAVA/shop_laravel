212fc30dcf2039540b7ca71a9ff4b72f172139b3
Alena Gontarenko <gontarenko.alena@gmail.com>
2020-09-24 11:50:23 +0300

    Add Seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            ['name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('admin'),
+                'is_admin' => 1],
+            ['name' => 'User1',
+                'email' => 'user1@example.com',
+                'password' => bcrypt('1qaz2wsx')],
+            ['name' => 'User2',
+                'email' => 'user2@example.com',
+                'password' => bcrypt('1qaz2wsx')],
+        ]);
+    }
+}
